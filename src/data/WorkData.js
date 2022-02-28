3b3276de8823802ee7324831b59a53f8d4f88e95
Fahimekermall <samiulbasirfahim360@hotmail.com>
2022-02-28 12:28:24 +0600

    first commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,14 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Sidebar Menu",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://react-sidebar.vercel.app/",
+        github:"https://github.com/FAHIMekermall/side-bar-1"
+       
+    }
+] 
